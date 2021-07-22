3134acb79be131f50e8eb63781f91cc5110a33df
강태훈 <xktmxkem@naver.com>
2021-07-22 23:41:12 +0900

    algorithm

--- algorithm/백만장자프로젝트.py
+++ algorithm/백만장자프로젝트.py
@@ -1,15 +1,25 @@
-test_case = int(input())
-for i in range(test_case):
+def recur(temp_list):
+    if len(temp_list) < 2:
+        print('끝')
+        return 0
+    else:
+        max_index = temp_list.index(max(temp_list))
+        finish_list = temp_list[:+1]
+        recur(temp_list[max_index:])
+        print(finish_list)
+        
 
-    day = int(input())
-    day_list = [0] * day
-    day_list = list(map(int, input().split()))
-    sum = 0
-    cnt = 0
-    for j in range(len(day_list)):
-        for x in range(len(day_list)):
-            if day_list[j] >= day_list[x]:
-                continue
-            else:
+
+recur([10, 7, 6])
+
+# test_case = int(input())
+# for i in range(test_case):
+
+#     day = int(input())
+#     day_list = [0] * day
+#     day_list = list(map(int, input().split()))
+#     sum = 0
+#     cnt = 0
+
                 
-    print(day_list)
+#     print(day_list)
--- python/0720/백만장자프로젝트.py
+++ python/0720/백만장자프로젝트.py
@@ -1,15 +1,15 @@
 test_case = int(input())
 for i in range(test_case):
-
     day = int(input())
     day_list = [0] * day
     day_list = list(map(int, input().split()))
-    sum = 0
+    max_index = day_list.index(max(day_list))
     cnt = 0
-    for j in range(len(day_list)):
-        for x in range(len(day_list)):
-            if day_list[j] >= day_list[x]:
-                continue
-            else:
+    sum = 0
+    for i in range(day):
+        if day_list[max_index] == day_list[day]:
+            
+        elif  & day_list[max_index+1:]
+            
                 
     print(day_list)

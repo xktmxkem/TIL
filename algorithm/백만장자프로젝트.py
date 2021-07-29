0cebff037c21bbb06f5d6ea0da151f51e68c1469
강태훈 <xktmxkem@naver.com>
2021-07-29 21:24:01 +0900

    modify exception

--- algorithm/백만장자프로젝트.py
+++ algorithm/백만장자프로젝트.py
@@ -1,25 +1,7 @@
-def recur(temp_list):
-    if len(temp_list) < 2:
-        print('끝')
-        return 0
-    else:
-        max_index = temp_list.index(max(temp_list))
-        finish_list = temp_list[:+1]
-        recur(temp_list[max_index:])
-        print(finish_list)
-        
+test = int(input())
 
-
-recur([10, 7, 6])
-
-# test_case = int(input())
-# for i in range(test_case):
-
-#     day = int(input())
-#     day_list = [0] * day
-#     day_list = list(map(int, input().split()))
-#     sum = 0
-#     cnt = 0
-
-                
-#     print(day_list)
+for test in range(test):
+    day = int(input())
+    day_ls = list(map(int, input().split()))
+    max_cost = max(day_ls)
+    print(max_cost)

38c29b70ad97f5ad9524385d0cbaee6f628bfdd9
강태훈 <xktmxkem@naver.com>
2021-08-08 21:18:58 +0900

    algorithm

--- algorithm/swea_d2/1928.py
+++ algorithm/swea_d2/1928.py
@@ -1,3 +1,21 @@
+import base64
+
+# def binary(num):
+#     binary_list = []
+#     while(1):
+#         if num < 2 :
+#             binary_list.append(1)
+#             break
+#         else:
+#             binary_list.append(num%2)
+#             num = int(num/2)
+#     binary_list.reverse()
+#     return(binary_list)
+
+
 test = int(input())
 for test in range(1, test + 1):
-    
+    str_encoding = input()
+    str_bytes = base64.b64decode(str_encoding)
+    str_decoding = str_bytes.decode('ascii')
+    print(f'#{test} {str_decoding}')

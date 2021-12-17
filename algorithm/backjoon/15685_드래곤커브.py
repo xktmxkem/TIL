06b4e9b6c5b85b8c6143f28fa692b6e758cf11b6
Admin <xktmxkem@naver.com>
2021-12-17 23:52:34 +0900

    refactoring algorithm

--- algorithm/backjoon/15685_드래곤커브.py
+++ algorithm/backjoon/15685_드래곤커브.py
@@ -4,7 +4,6 @@ info = [list(map(int, input().split())) for _ in range(N)]
 MAP = [[0 for i in range(101)] for j in range(101)]
 for i in info:
     x, y, d, g = i
-    draw(x, y)
-
+    a, b, c, d = i
 for i in MAP:
     print(i)

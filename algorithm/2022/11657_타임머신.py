bb3b6b6d878adb4accd99dc9b8ae1e49f3d127a3
unknown <xktmxkem@naver.com>
2022-08-25 21:05:05 +0900

    modify algorithm'

--- algorithm/2022/11657_타임머신.py
+++ algorithm/2022/11657_타임머신.py
@@ -1,34 +1,31 @@
-import sys
-input = sys.stdin.readline
-edge = []
-n, m = map(int, input().split())
+n, m  = map(int, input().split())
+town = []
 for i in range(m):
-    s, e, c= map(int, input().split())
-    edge.append([s, e, c])
-INF = float("inf")
+    s, e, c = map(int ,input().split())
+    town.append([s, e, c])
+INF = float('inf')
 distance = [INF for i in range(n + 1)]
 
-def ford(start):
+def fold(start):
     global n, m
     distance[start] = 0
+
     for i in range(n):
         for j in range(m):
-            now = edge[j][0]
-            next = edge[j][1]
-            cost = edge[j][2]
-            # 현재 간선을
-            if distance[now] != INF and distance[next] > distance[now]:
+            now, next, cost = town[j]
+
+            if distance[now] != INF and distance[next] > distance[now] + cost:
                 distance[next] = distance[now] + cost
-                if i == n - 1:
+                if i == n -1:
                     return True
     return False
 
-check = ford(1)
-if check:
-    print("-1")
+ans = fold(1)
+if ans:
+    print('-1')
 else:
-    for i in range(2, n + 1):
-        if distance[i] ==  INF:
-            print(-1)
-        else:
-            print(distance[i])
+    for i in range(2, len(distance)):
+            if distance[i] == INF:
+                print('-1')
+            else:
+                print(distance[i])
